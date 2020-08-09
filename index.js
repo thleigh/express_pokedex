ffd76b81eec9644baa550012c3942f399b4df95b
Tanner Leigh <tannerhleigh@gmail.com>
2020-08-09 16:08:52 -0700

    [express] create favorites.ejs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios'); 
 const ejsLayouts = require('express-ejs-layouts');
+const db = require('./models');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,10 +12,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 // GET - main index of site
-app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+app.get('/', (req, res) => {
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151/';
   // Use request to call the API
   axios.get(pokemonUrl).then(response => {
+    // console.log(response.data.results.name[1]) 
     let pokemon = response.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   });
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+// Make sure to require your models in the files where they will be used.
+var db = require('../models');
+const { parse } = require('dotenv');
+const { response } = require('express');
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', function(req, res) {
+router.get('/', (req, res) => { 
+
+  let favPoke = db.pokemon.findAll()
   // TODO: Get all records from the DB and render to view
   res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/', (req, res) => {
   // TODO: Get form data and add a new record to DB
   res.send(req.body);
 });
